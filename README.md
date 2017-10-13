1a29e933726282fa665b92d9bd30a64a52977035
Unknown <lzj1995@126.com>
2017-10-13 11:12:56 +0800

    Add support for multiple playlists

--- README.md
+++ README.md
@@ -9,11 +9,13 @@ Fill config.yml with your own info:
 
 ```yaml
 user_id: your-spotify-account-id
-playlist_id: your-spotify-playlist-id
 client_id: your-spotify-app-client-id
 client_secret: your-spotify-app-client-secret
 redirect_uri: your-spotify-app-redirect-uri
-chart_url: https://spotifycharts.com/regional/global/daily/latest/download
+generator:
+# region: playlist_id || chart_url
+  global: your-global-top-200-playlist-id || https://spotifycharts.com/regional/global/daily/latest/download
+  us: your-us-top-200-playlist-id || https://spotifycharts.com/regional/us/daily/latest/download
 ```
 
  **Where to get user_id and playlist_id?**
@@ -31,12 +33,6 @@ In this case, user_id is `12135742379` and playlist_id is `6F61J2pfnBeVZkXfumFQI
 Go [here](https://developer.spotify.com/my-applications) to create your own Spotify application and get all these stuff.
 
 Then, you can run the script. During the runtime, you need to authorize your Spotify account for your Spotify application if you have never done this before. Follow the [instruction](http://spotipy.readthedocs.io/en/latest/#authorized-requests) to get authorized. The credentials will be cached in local file `.cache-*` which can be used to automatically re-authorize expired tokens, so you won't need to go through this procedure next time.
-
-The data source is [Spotify Charts](https://spotifycharts.com). I use the global chart by default, but you can change it if you want. For example, if you want to use USA chart, change `chart_url` in config.yml to:
-
-```yaml
-chart_url: https://spotifycharts.com/regional/us/daily/latest/download
-```
 
 ### Auto Update Playlist Daily
 
--- generate_top_200_playlist.py
+++ generate_top_200_playlist.py
@@ -12,32 +12,34 @@ except FileNotFoundError as e:
     config_file = open('config.yml', 'r')
 finally:
     config = yaml.load(config_file)
-    user_id = str(config['user_id'])
-    playlist_id = config['playlist_id']
-    client_id = config['client_id']
-    client_secret = config['client_secret']
-    redirect_uri = config['redirect_uri']
-    chart_url = config['chart_url']
+    user_id = str(config['user_id']).strip()
+    client_id = str(config['client_id']).strip()
+    client_secret = str(config['client_secret']).strip()
+    redirect_uri = str(config['redirect_uri']).strip()
+    generator = config['generator']
 
 scope = 'playlist-modify-public'
 
 token = util.prompt_for_user_token(user_id, scope, client_id, client_secret, redirect_uri)
 
-r = requests.get(chart_url)
-tracks = r.text.splitlines()[1:]
-
-track_ids = []
-for line in tracks:
-    track_ids.append(line.rpartition('/')[2])
-
 if token:
     sp = spotipy.Spotify(auth=token)
     sp.trace = False
-    # You can add a maximum of 100 tracks per request.
-    results1 = sp.user_playlist_replace_tracks(user_id, playlist_id, track_ids[:100])
-    print(results1, time.strftime(ISO_TIME_FORMAT, time.localtime()))
-    results2 = sp.user_playlist_add_tracks(user_id, playlist_id, track_ids[100:])
-    print(results2, time.strftime(ISO_TIME_FORMAT, time.localtime()))
+    for region, info in generator.items():
+        playlist_id = str(info).partition('||')[0].strip()
+        chart_url = str(info).partition('||')[2].strip()
+
+        r = requests.get(chart_url)
+        tracks = r.text.splitlines()[1:]
+        track_ids = []
+        for line in tracks:
+            track_ids.append(line.rpartition('/')[2])
+
+        # You can add a maximum of 100 tracks per request.
+        results1 = sp.user_playlist_replace_tracks(user_id, playlist_id, track_ids[:100])
+        print(region, results1, time.strftime(ISO_TIME_FORMAT, time.localtime()))
+        results2 = sp.user_playlist_add_tracks(user_id, playlist_id, track_ids[100:])
+        print(region, results2, time.strftime(ISO_TIME_FORMAT, time.localtime()))
 
 else:
     print("Can't get token for", user_id)
