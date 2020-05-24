1dfb9957f53f96cba166564866b837755de9cbfd
Zijian Liu <liu.zijian@outlook.com>
2020-05-24 05:08:08 -0400

    add html parser

--- generate_top_200_playlist.py
+++ generate_top_200_playlist.py
@@ -5,6 +5,7 @@ import time
 import yaml
 import sys
 import os
+from lxml import html
 
 ISO_TIME_FORMAT = '%Y-%m-%d %X'
 project_dir = os.path.split(os.path.realpath(sys.argv[0]))[0]
@@ -27,8 +28,8 @@ scope = 'playlist-modify-public'
 token = util.prompt_for_user_token(user_id, scope, client_id, client_secret, redirect_uri)
 
 
-def get_chart_url(region_code):
-    return 'https://spotifycharts.com/regional/' + region_code + '/daily/latest/download'
+def get_chart_page(region_code):
+    return 'https://spotifycharts.com/regional/' + region_code + '/daily/latest'
 
 
 if token:
@@ -36,21 +37,29 @@ if token:
     sp.trace = False
     for (region, playlist_id) in generator.items():
         playlist_id = str(playlist_id).strip()
-        chart_url = get_chart_url(region)
+        chart_page = get_chart_page(region)
 
-        r = requests.get(chart_url)
-        lines = r.text.splitlines()
-        track_ids = []
-        for line in lines:
-            if line[0].isdigit():
-                track_ids.append(line.rpartition('/')[2])
+        r = requests.get(chart_page)
+        if r.status_code == 200:
+            tree = html.fromstring(r.content)
+            selected_values = tree.xpath('//div[@class="responsive-select-value"]/text()')
+            country = selected_values[0]
+            recurrence = selected_values[1].capitalize()
+            date = selected_values[2]
 
-        # You can add a maximum of 100 tracks per request.
-        sp.user_playlist_replace_tracks(user_id, playlist_id, [])
-        results1 = sp.user_playlist_add_tracks(user_id, playlist_id, track_ids[:100])
-        print(region, results1, time.strftime(ISO_TIME_FORMAT, time.localtime()))
-        results2 = sp.user_playlist_add_tracks(user_id, playlist_id, track_ids[100:])
-        print(region, results2, time.strftime(ISO_TIME_FORMAT, time.localtime()))
+            track_urls = tree.xpath('//td[@class="chart-table-image"]/a/@href')
+            track_ids = []
+            for url in track_urls:
+                track_ids.append(url.rpartition('/')[2])
+            sp.user_playlist_change_details(user_id, playlist_id,
+                                            ' '.join(['Spotify Top 200', country, date]))
+
+            sp.user_playlist_replace_tracks(user_id, playlist_id, [])
+            # You can add a maximum of 100 tracks per request.
+            results1 = sp.user_playlist_add_tracks(user_id, playlist_id, track_ids[:100])
+            print(region, results1, time.strftime(ISO_TIME_FORMAT, time.localtime()))
+            results2 = sp.user_playlist_add_tracks(user_id, playlist_id, track_ids[100:])
+            print(region, results2, time.strftime(ISO_TIME_FORMAT, time.localtime()))
 
 else:
     print("Can't get token for", user_id)
