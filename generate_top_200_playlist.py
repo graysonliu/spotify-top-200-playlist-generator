7300ebd9888fba5588559202bac72fceeaec66f3
Zijian Liu <liu.zijian@outlook.com>
2020-10-14 02:36:05 -0400

    :running:

--- generate_top_200_playlist.py
+++ generate_top_200_playlist.py
@@ -18,6 +18,7 @@ client_secret = os.getenv('SPOTIPY_CLIENT_SECRET')
 
 # for github actions, create .cache file from the secret, which is set to the environment variable SPOTIPY_AUTH_CACHE
 auth_cache = os.getenv('SPOTIPY_AUTH_CACHE')
+print(auth_cache)
 if auth_cache:
     with open('.cache', 'w') as f:
         f.write(auth_cache)
