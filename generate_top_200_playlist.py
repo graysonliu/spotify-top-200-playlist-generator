ea29ad14004b64a1578efa43d78f847a1d62530a
Zijian Liu <liu.zijian@outlook.com>
2020-10-14 02:41:14 -0400

    :running:

--- generate_top_200_playlist.py
+++ generate_top_200_playlist.py
@@ -16,13 +16,6 @@ load_dotenv()
 client_id = os.getenv('SPOTIPY_CLIENT_ID')
 client_secret = os.getenv('SPOTIPY_CLIENT_SECRET')
 
-# for github actions, create .cache file from the secret, which is set to the environment variable SPOTIPY_AUTH_CACHE
-auth_cache = os.getenv('SPOTIPY_AUTH_CACHE')
-print(auth_cache)
-if auth_cache:
-    with open('.cache', 'w') as f:
-        f.write(auth_cache)
-
 ISO_TIME_FORMAT = '%Y-%m-%d %X'
 
 with open('config.yml', 'r') as config_file:
